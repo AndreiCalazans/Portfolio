81cdbd21f8d9fe9d356fedfbeda9cd3d19de3b9b
AndreiCalazans <andreixoc@hotmail.com>
2017-04-14 09:44:20 -0300

    production set up

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,11 @@ module.exports = {
   },
   plugins: [
     extractPlugin,
+    new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+}),
     new webpack.optimize.UglifyJsPlugin({
     compress: {
         warnings: false
